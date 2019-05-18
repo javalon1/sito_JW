99092be6dd65d5785b5e1fcf20eb167e3ad92cf3
Jacek.Woj <jacek.wojtuszek@gmail.com>
2019-05-18 12:53:38 +0100

    metoda_naiwna

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,9 +1,22 @@
 package com.sda.algorytmy;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int k = 2;
+        while (k < 200) {
+            int licznik = 0;
+
+            for (int i = 2; i < k; i++) {
+                if (k % i == 0) {
+                    licznik++;
+                }
+            }
+            if (licznik == 0) {
+                System.out.println(k + "jest liczba pierwsz");
+            } else {
+
+            }
+            k++;
+        }
     }
 }
